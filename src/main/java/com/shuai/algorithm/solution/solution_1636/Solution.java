e350dc27fdff4faf9485715d4c67044bc7280a1e
Yangs <296077244@qq.com>
2022-09-19 23:05:06 +0800

    1636. 按照频率将数组升序排序

---
+++ src/main/java/com/shuai/algorithm/solution/solution_1636/Solution.java
@@ -0,0 +1,48 @@
+package com.shuai.algorithm.solution.solution_1636;
+
+import java.util.*;
+
+/**
+ * 给你一个整数数组nums，请你将数组按照每个值的频率升序排序。如果有多个值的频率相同，请你按照数值本身将它们降序排序
+ *
+ * 请你返回排序后的数组。
+ *
+ * 输入：nums = [1,1,2,2,2,3]
+ * 输出：[3,1,1,2,2,2]
+ * 解释：'3' 频率为 1，'1' 频率为 2，'2' 频率为 3 。
+ *
+ * 输入：nums = [2,3,1,3,2]
+ * 输出：[1,3,3,2,2]
+ * 解释：'2' 和 '3' 频率都为 2 ，所以它们之间按照数值本身降序排序。
+ *
+ * 输入：nums = [-1,1,-6,4,5,-6,1,4,1]
+ * 输出：[5,-1,4,4,-6,-6,1,1,1]
+ * */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 艹, 官方题解也是暴力模拟。。。。
+     * */
+    public static int[] frequencySort(int[] nums) {
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (int num : nums) {
+            cnt.put(num, cnt.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+        list.sort((a, b) -> {
+            int cnt1 = cnt.get(a), cnt2 = cnt.get(b);
+            return cnt1 != cnt2 ? cnt1 - cnt2 : b - a;
+        });
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            nums[i] = list.get(i);
+        }
+        return nums;
+    }
+}
